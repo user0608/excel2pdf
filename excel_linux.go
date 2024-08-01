1e260906feda496ba7020c2b7b5cc076df552fe9
Kevin Sacuedo <user002_@outlook.com>
2024-08-01 18:58:18 -0500

    fix findLibreOfficeBinPath on linux

--- excel_linux.go
+++
@@ -1,6 +0,0 @@
-//go:build linux || darwin
-// +build linux darwin
-
-package excel2pdf
-
-func isExcelInstalled() (bool, error) { return false, nil }
--- libreoffice_linux.go
+++ libreoffice_linux.go
@@ -7,23 +7,19 @@ import (
 	"errors"
 	"log/slog"
 	"os/exec"
+	"strings"
 )
 
 var ErrLibreofficeNotInstalled = errors.New("LibreOffice is not installed")
 
 func findLibreOfficeBinPath() (string, error) {
 	cmd := exec.Command("which", "libreoffice")
-
-	if err := cmd.Run(); err != nil {
-		slog.Error("running which libreoffice", "error", err)
-		return "", ErrLibreofficeNotInstalled
-	}
-	out, err := cmd.Output()
+	out, err := cmd.CombinedOutput()
 	if err != nil {
 		slog.Error("reading output `which libreoffice`", "error", err)
 		return "", ErrLibreofficeNotInstalled
 	}
-	var libreofficePath = string(out)
+	var libreofficePath = strings.TrimSpace(string(out))
 	if libreofficePath == "" {
 		return libreofficePath, ErrLibreofficeNotInstalled
 	}
