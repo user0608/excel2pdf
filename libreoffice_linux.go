35f4db007477992f89201dcb1f82d74ae45199de
Kevin Sacuedo <user002_@outlook.com>
2024-10-17 16:47:16 -0500

    libre office variable y libreoffice24.8

--- libreoffice_linux.go
+++ libreoffice_linux.go
@@ -6,13 +6,14 @@ package excel2pdf
 import (
 	"errors"
 	"log/slog"
+	"os"
 	"os/exec"
 	"strings"
 )
 
 var ErrLibreofficeNotInstalled = errors.New("LibreOffice is not installed")
 
-func findLibreOfficeBinPath() (string, error) {
+func findLibreOffice() (string, error) {
 	cmd := exec.Command("which", "libreoffice")
 	out, err := cmd.CombinedOutput()
 	if err != nil {
@@ -20,6 +21,35 @@ func findLibreOfficeBinPath() (string, error) {
 		return "", ErrLibreofficeNotInstalled
 	}
 	var libreofficePath = strings.TrimSpace(string(out))
+	return libreofficePath, nil
+}
+
+func findlibreoffice24_8() (string, error) {
+	cmd := exec.Command("which", "libreoffice24.8")
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		slog.Error("reading output `which libreoffice`", "error", err)
+		return "", ErrLibreofficeNotInstalled
+	}
+	var libreofficePath = strings.TrimSpace(string(out))
+	return libreofficePath, nil
+}
+
+func findLibreOfficeBinPath() (string, error) {
+	value, ok := os.LookupEnv("LIBREOFFICE_PATH")
+	if ok {
+		return value, nil
+	}
+	libreofficePath, err := findLibreOffice()
+	if err != nil {
+		return "", err
+	}
+	if libreofficePath == "" {
+		libreofficePath, err = findlibreoffice24_8()
+		if err != nil {
+			return "", err
+		}
+	}
 	if libreofficePath == "" {
 		return libreofficePath, ErrLibreofficeNotInstalled
 	}
